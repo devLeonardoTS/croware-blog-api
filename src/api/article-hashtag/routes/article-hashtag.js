a7bb0e236c45529f5947fb63c8d399e2e1be7221
Leonardo T. S <leonardo_takanobu@hotmail.com>
2022-09-01 18:23:27 -0300

    Initial setup done

---
+++ src/api/article-hashtag/routes/article-hashtag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-hashtag router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::article-hashtag.article-hashtag');
---
+++ src/api/article-hashtag/services/article-hashtag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-hashtag service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::article-hashtag.article-hashtag');
